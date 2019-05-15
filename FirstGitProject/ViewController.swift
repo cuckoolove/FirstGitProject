8155fcd2a84783641870b0c18bc4f8d2a4697df8
Savitri <savitri25@gmail.com>
2019-05-15 08:27:20 +0700

    Add save coordinate

---
+++ FirstGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstGitProject
+//
+//  Created by Savitri Nurhayati on 15/05/19.
+//  Copyright © 2019 Savitri Nurhayati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
